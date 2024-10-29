cbb50ffc544652ac0c763fa0ce636aaec82c47db
RafZen <Rafal.Szabelski@zenergi.co.uk>
2024-10-29 13:29:33 +0000

    Add project files.

---
+++ CustomerPortal/Pages/DebugTest.razor.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+
+namespace CustomerPortal.Pages
+{
+    public partial class DebugTest:ComponentBase
+    {
+      
+        protected override  async Task OnInitializedAsync()
+        {
+            await Task.Delay(1);
+        }
+    }
+}
+
