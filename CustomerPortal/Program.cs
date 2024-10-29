0cb130eacd2371909dd9a2ba1d3252e3dc444526
RafZen <Rafal.Szabelski@zenergi.co.uk>
2024-10-29 14:45:01 +0000

    Initial

--- CustomerPortal/Program.cs
+++ CustomerPortal/Program.cs
@@ -10,20 +10,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-//builder.Services.AddHttpClient("ServerAPI",
-//      client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-//    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-//builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-//  .CreateClient("ServerAPI"));
-
-
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
-    options.ProviderOptions.ResponseType = "code";
-    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
-
+    options.ProviderOptions.ResponseType = "code";    
 });
 
 
