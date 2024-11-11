cf858fe46ca29a2bf01549b9c1ab5c55a6c95f3f
RafZen <Rafal.Szabelski@zenergi.co.uk>
2024-11-11 11:27:27 +0000

    Okta made active

--- CustomerPortal/Program.cs
+++ CustomerPortal/Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
-    options.ProviderOptions.ResponseType = "code";    
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
 });
 
 
